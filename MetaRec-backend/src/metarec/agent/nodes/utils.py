ec80d55698b71152b21af56013564489fe00e8ec
briyanii <e0310729@u.nus.edu>
2026-04-15 19:44:12 +0800

    Add graph_node decorator to add names to node functions

--- MetaRec-backend/src/metarec/agent/nodes/utils.py
+++ MetaRec-backend/src/metarec/agent/nodes/utils.py
@@ -1,4 +1,24 @@
 from ..state import AgentState
+import inspect
+import asyncio
+import functools
+
+def graph_node(name: str):
+    def decorator(func):
+        
+        if inspect.iscoroutinefunction(func):
+            @functools.wraps(func)
+            async def wrapper(*args, **kwargs):
+                return await func(*args, **kwargs)
+        else:
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                return func(*args, **kwargs)
+
+        func.name = name
+        wrapper.name = name
+        return wrapper
+    return decorator
 
 def no_op(state: AgentState, config):
     """
